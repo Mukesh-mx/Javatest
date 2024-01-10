0131b0323095d3acdbcf7af819adcc0eb93e52ce
mukesh <mukesh@localhost>
2024-01-11 00:19:17 +0530

    initial commit1

--- src/test_programs/First.java
+++ src/test_programs/First.java
@@ -1,11 +1,21 @@
 package test_programs;
 
+import java.util.Scanner;
+
 public class First {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int totalSamples = 0;
+        int rounds = 0;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("HELLO");
+        while (totalSamples < 30) {
+            System.out.print("Enter the number of leaf samples collected in round " + (rounds + 1) + ": ");
+            int samplesCollected = scanner.nextInt();
+            totalSamples += samplesCollected;
+            rounds++;
+        }
 
-	}
-
+        System.out.println("Total samples collected: " + totalSamples);
+        System.out.println("Number of rounds: " + rounds);
+    }
 }
