f0d846f9029845ff946ab3d48352b815d4e56871
Mukesh <55647823+Mukesh-mx@users.noreply.github.com>
2024-01-11 13:34:18 +0530

    Update Third

--- src/test_programs/Third.java
+++ src/test_programs/Third.java
@@ -1,11 +1,26 @@
 package test_programs;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Third {
     public static void main(String[] args) {
         String[] numberNames = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in); 
+        
+        int randomIndex = random.nextInt(numberNames.length);
+        String randomNumberName = numberNames[randomIndex];
 
-        for (int i = 0; i <= 9; i++) {
-            System.out.print(numberNames[i] + " ");
+        System.out.println("Type the number names for " + randomIndex);
+        String userInput = scanner.next();
+
+        if (userInput.trim().equalsIgnoreCase(randomNumberName)) {
+            System.out.println("Correct!");
+        } 
+        else {
+            System.out.println("Incorrect. The correct answer is: " + randomNumberName);
         }
     }
 }
